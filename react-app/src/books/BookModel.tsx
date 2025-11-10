cb55723c8d4272f6bfcd5f66f412dea42da08bde
Mathis-Ru <mathis.ruchot@student.junia.com>
2025-11-10 16:31:20 +0100

    Modification du bookModel

--- react-app/src/books/BookModel.tsx
+++ react-app/src/books/BookModel.tsx
@@ -1,18 +1,44 @@
+export type AuthorModel = {
+  id: string
+  firstName: string
+  lastName: string
+}
+
 export type BookModel = {
   id: string
   title: string
   yearPublished: number
-  author: {
-    id: string
-    firstName: string
-    lastName: string
-  }
+  author: AuthorModel
+  photo?: string 
 }
 
 export type CreateBookModel = {
   authorId: string
   title: string
   yearPublished: number
+  photo?: string 
 }
 
 export type UpdateBookModel = Partial<CreateBookModel>
+
+
+type Purchaser = {
+  id: number // ID du client
+  firstName: string
+  lastName: string
+  purchaseDate: string // La date d'achat de CE livre
+}
+
+export interface BookDetailsModel extends BookModel {
+  purchasers: Purchaser[] 
+}
+
+export interface BookListModel extends BookModel {
+  purchasersCount: number
+}
+
+
+export type CreateSaleModel = {
+  clientId: number 
+  date: string // La date de l'achat (format ISO)
+}
