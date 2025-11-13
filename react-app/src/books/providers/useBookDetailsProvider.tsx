f81fea44c4a16f0e3a94c4a3461d68f6ffc61bb8
nilsl <nils.lesage@gmail.com>
2025-11-13 20:08:36 +0100

    ventes d'un livre depuis detail du livre

--- react-app/src/books/providers/useBookDetailsProvider.tsx
+++ react-app/src/books/providers/useBookDetailsProvider.tsx
@@ -8,13 +8,30 @@ export const useBookDetailsProvider = (id: string) => {
   const [isLoading, setIsLoading] = useState(false)
   const [book, setBook] = useState<BookModel | null>(null)
 
-  const loadBook = () => {
+  const loadBook = async () => {
     setIsLoading(true)
-    fetch(`${API_URL}/${id}`)
-      .then(response => response.json())
-      .then(data => setBook(data))
-      .finally(() => setIsLoading(false))
+    try {
+      const [bookRes, salesRes] = await Promise.all([
+        fetch(`${API_URL}/${id}`),
+        fetch(`${API_BASE_URL}/sales/book/${id}/clients`)
+      ])
+
+      const [bookData, purchasersData] = await Promise.all([
+        bookRes.json(),
+        salesRes.json()
+      ])
+
+      setBook({
+        ...bookData,
+        purchasers: purchasersData
+      })
+    } catch (error) {
+      console.error('Erreur lors du chargement du livre :', error)
+    } finally {
+      setIsLoading(false)
+    }
   }
+
 
   return { isLoading, book, loadBook }
 }
